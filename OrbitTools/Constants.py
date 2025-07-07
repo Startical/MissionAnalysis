aab4a58fcea44340b19025504b59b3bdca756af7
asier-ah <aahijon@startical.com>
2025-07-07 12:04:21 +0200

    added speed of light to constants

--- OrbitTools/Constants.py
+++ OrbitTools/Constants.py
@@ -7,3 +7,6 @@ EARTH_RADIUS = 6378.0  # in km
 EARTH_MASS = 5.972e24  # in kg
 EARTH_MU = 398600.4418  # in km^3/s^2
 EARTH_W = 2*np.pi/(23*3600+56*60+4)
+
+#Speed of light
+c_light = 299792458 # in m/s
